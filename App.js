1180cebf078f3bfb6c45263e5487cca9096bc921
Akash_Kharat <akashkharat1339@gmail.com>
2023-01-04 18:23:20 +0530

    done

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Todo List</Text>
+        <View style={styles.items}></View>
+      </View>
     </View>
   );
 }
